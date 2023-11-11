fdc5d0dae8bf236a20a3defbcb9305c064096129
Jesse Nay <jessenay26@gmail.com>
2023-11-11 12:25:46 -0700

    Added a highscores page and added some buttons to it

---
+++ questions.js
@@ -0,0 +1,31 @@
+var questions = [
+    {
+        questiontitle: "Inside which HTML element do we put the JavaScript?",
+        choices: ["<scripting>", "<js>", "<script>", "<javascript>"],
+        answer: "<script>"
+    },
+
+    {
+        questiontitle: "How do you create a function in JavaScript?",
+        choices: ["function:myFunction()", "function = myFunction()", "function ~ myFunction()", "function myFunction()"],
+        answer: "function myFunction()"
+    },
+
+    {
+        questiontitle: "How can you add a single line comment in Javascript?",
+        choices: ["//This is a comment", "`This is a comment`", "<!--This is a comment-->", "/*This is a comment*/"],
+        answer: "//This is a comment"
+    },
+
+    {
+        questiontitle: "Which operator is used to assign a value to a variable?",
+        choices: ["-", "*", "=", "x"],
+        answer: "="
+    },
+
+    {
+        questiontitle: "How do you declare a JavaScript variable?",
+        choices: ["var carName;", "variable carName;", "v carName;", "vrbl carName;"],
+        answer: "var carName;"
+    },
+];
